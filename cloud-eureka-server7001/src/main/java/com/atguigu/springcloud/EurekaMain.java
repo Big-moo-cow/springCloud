f6bc3b8323a66d31324b334f40cad3c80300c53b
Cza <17554238739@163.com>
2020-05-22 15:48:18 +0800

    eureka单例 以及公共类抽取

---
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: 车子昂
+ * @Date: 2020-4-11 14:08
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain.class,args);
+    }
+}
